bfd08d96ad610bf8ad58dc56b3797c9d7837b7a9
Felix Eyetan <felix.eyetan@thomsonreuters.com>
2017-10-20 10:20:38 +0100

    Update data source url

--- js/test-collaboration-map.js
+++ js/test-collaboration-map.js
@@ -1,10 +1,10 @@
-$(function(){
-  plotCollaborationMap();
+$(function() {
+    plotCollaborationMap();
 });
-function plotCollaborationMap()
-{
-  d3.json("/data/fpa-data.json", function(dataJson) {
-  	var model = new FPAModel(dataJson);
-    var plot = new CollaborationMap("graph", "graph-info", model.getCollaborationData());
-  });
+
+function plotCollaborationMap() {
+    d3.json("./data/fpa-data.json", function(dataJson) {
+        var model = new FPAModel(dataJson);
+        var plot = new CollaborationMap("graph", "graph-info", model.getCollaborationData());
+    });
 }
